1e40fd210fe47494a52b1c1d723b5b6dc8385968
andersonk-cit <andersonk@ciandt.com>
2021-10-05 09:54:47 -0300

    5 files modified, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component, VERSION } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Library {
+  name: string;
+  latest: string;
+}
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  titleText = "Angular Material Playground " + VERSION.major;
+export class AppComponent implements OnInit {
+  searchValue: string;
+  readonly CDN_API_URL = 'https://api.cdnj.com/libraries';
+  results$: Observable<any>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onSearch() {
+    console.log();
+
+    this.results$ = this.http.get(this.CDN_API_URL);
+  }
 }
